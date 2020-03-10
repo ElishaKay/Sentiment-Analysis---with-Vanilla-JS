2e3e29490b6ff040ff7193d198507ee1383ce926
Elisha Kramer <kramer1346@gmail.com>
2020-03-10 20:42:03 +0200

    calculating score for sample article

--- populateDB.js
+++ populateDB.js
@@ -6,11 +6,35 @@ let companies = JSON.parse(fs.readFileSync('companies.json'));
 
 let blogpost = JSON.parse(fs.readFileSync('blogpost.json'));
 
-console.log('afinn: ',afinn);
-console.log('companies: ',companies);
-console.log('blogpost: ',blogpost);
+// console.log('afinn: ',afinn);
+// console.log('companies: ',companies);
+// console.log('blogpost: ',blogpost);
 
 
+var words = blogpost['text'].split(/\W/);
+// console.log(words);
+
+var scoredWords = [];
+var totalScore = 0;
+for (var i = 0; i < words.length; i++) {
+  var word = words[i].toLowerCase();
+  if (afinn.hasOwnProperty(word)) {
+    var score = afinn[word];
+    totalScore += Number(score);
+    scoredWords.push(word + ': ' + score + ' ');
+  }
+}
+
+console.log('totalScore: ', totalScore);
+console.log('scoredWords: ',scoredWords);
+
+
+// var scorePar = select('#scoreP');
+// scorePar.html('score: ' + totalScore);
+// var comp = select('#comparativeP');
+// comp.html('comparative: ' + totalScore / words.length);
+// var wordlist = select('#wordlistP');
+// wordlist.html(scoredwords);
 
 
 
